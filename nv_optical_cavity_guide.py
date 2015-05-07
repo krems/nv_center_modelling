b98b6fd35c0a4b46b6137566aab7687dcd309b7c
krems <kremsnx@gmail.com>
2015-05-07 22:05:59 +0300

    minors

--- nv_optical_cavity_guide.py
+++ nv_optical_cavity_guide.py
@@ -6,18 +6,18 @@ import matplotlib.pyplot as plt
 
 # constants
 E = 7.7 * (10 ** -3)  # |-1> |1> distance
-gama = 2.25 * (10 ** 3)  # dissipation from NV
-kappa = 5 * (10 ** 3)  # dissipation from cavity
-gm = 10 ** 3  # coupling wave-guide with cavity
-# gm = 0
-# gama = 0
-# kappa = 0
+# gama = 2.25 * (10 ** 3)  # dissipation from NV
+# kappa = 5 * (10 ** 3)  # dissipation from cavity
+# gm = 10 ** 3  # coupling wave-guide with cavity
+gm = 0
+gama = 0
+kappa = 0
 h = 1  # Plank
 mu = 2.0028 * 13.99624 * (10 ** 0)  # electron ~ nv
 B_z = 500.0  # magnetic field to split |1> |-1> spin states
 
-w_p = 4.706 * (10 ** 5) - h * mu * B_z  # e->u transition frequency 10**14
-w_m = w_p + h * E + h * mu * B_z  # e->g transition frequency
+w_p = 4.706 * (10 ** 5) - h * mu * B_z  # e<->u transition frequency 10**14
+w_m = w_p + h * E + h * mu * B_z  # e<->g transition frequency
 w_g = w_p + 2 * 10 ** 4  # cavity frequency
 w_c = w_m - 2 * 10 ** 4  # cavity frequency
 delta = 0
@@ -28,12 +28,12 @@ omega_p = 100.0 * (10 ** 4)
 
 def plot_populations(dt, ee, eg, eu, ge, gg, gu, t0, t1):
     x = linspace(t0, t1, (t1 - t0) / dt + 10)
-    plt.plot(x, ee, "red", label="ee", linestyle='--')
+    # plt.plot(x, ee, "red", label="ee", linestyle='--')
     plt.plot(x, eg, "blue", label="e+")
     plt.plot(x, eu, "green", label="e-")
     plt.plot(x, ge, "orange", label="ge", linewidth=2, linestyle='--')
-    plt.plot(x, gg, "black", label="g+", linewidth=2)
-    plt.plot(x, gu, "gray", label="g-", linewidth=2)
+    # plt.plot(x, gg, "black", label="g+", linewidth=2)
+    # plt.plot(x, gu, "gray", label="g-", linewidth=2)
     plt.show()
 
 
@@ -58,18 +58,18 @@ def integrate(dt, r, t0, t1):
 # Schrodinger equation's
 def right_part(t, y):
     hamiltonian = array(
-        [[1.5 * w_c - 1j * kappa, -.5 * omega_p * exp(-1j * t * (w_p - w_g)) - gm * exp(-1j * t * (w_c - w_g)), -.5 * omega_p * exp(-1j * t * (w_m - w_g)),
+        [[1.5 * w_c - 1j * kappa, -.5 * omega_p * exp(-1j * t * (w_p - w_g)) - 1j * gm * exp(-1j * t * (w_c - w_g)), -.5 * omega_p * exp(-1j * t * (w_m - w_g)),
           0., 0., 0.],
-         [-.5 * omega_p * exp(1j * t * (w_p - w_g)) - gm * exp(1j * t * (w_c - w_g)), 1.5 * w_c - 1j * kappa - w_p, 0.,
+         [-.5 * omega_p * exp(1j * t * (w_p - w_g)) + 1j * gm * exp(1j * t * (w_c - w_g)), 1.5 * w_c - 1j * kappa - w_p, 0.,
           -.5 * omega_m * exp(1j * t * (w_p - w_c)), 0., 0.],
          [-.5 * omega_p * exp(1j * t * (w_m - w_g)), 0., 1.5 * w_c - 1j * kappa + delta - w_m - 1j * gama,
-          -.5 * omega_m * exp(1j * t * (w_m - w_c)) - gm * exp(-1j * t * (w_c - w_g)), 0., 0.],
-         [0., -.5 * omega_m * exp(-1j * t * (w_p - w_c)), -.5 * omega_m * exp(-1j * t * (w_m - w_c)) - gm * exp(1j * t * (w_c - w_g)),
+          -.5 * omega_m * exp(1j * t * (w_m - w_c)) - 1j * gm * exp(-1j * t * (w_c - w_g)), 0., 0.],
+         [0., -.5 * omega_m * exp(-1j * t * (w_p - w_c)), -.5 * omega_m * exp(-1j * t * (w_m - w_c)) + 1j * gm * exp(1j * t * (w_c - w_g)),
           .5 * w_c, -.5 * omega_p * exp(-1j * t * (w_p - w_g)), -.5 * omega_p * exp(-1j * t * (w_m - w_g))],
          [0., 0., 0.,
-          -.5 * omega_p * exp(1j * t * (w_p - w_g)), .5 * w_c - w_p, - gm * exp(-1j * t * (w_c - w_g))],
+          -.5 * omega_p * exp(1j * t * (w_p - w_g)), .5 * w_c - w_p, - 1j * gm * exp(-1j * t * (w_c - w_g))],
          [0., 0., 0.,
-          -.5 * omega_p * exp(1j * t * (w_m - w_g)),  -gm * exp(1j * t * (w_c - w_g)), .5 * w_c + delta - w_m - 1j * gama]], dtype=complex128)
+          -.5 * omega_p * exp(1j * t * (w_m - w_g)),  1j * gm * exp(1j * t * (w_c - w_g)), .5 * w_c + delta - w_m - 1j * gama]], dtype=complex128)
     hamiltonian *= -1j * h
     return dot(hamiltonian, y)
 
